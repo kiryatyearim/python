2463f4088d126396a1f712f8d450f002219e337b
kiryatyearim <34747756+kiryatyearim@users.noreply.github.com>
2023-09-15 13:53:58 -0400

    Update post-webjook.py

--- post-webjook.py
+++ post-webjook.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     print( "Insode /webhook endpoint", end=' ', flush=True )
+    sys.stdout.flush()
+    time.sleep(.2)  # or other time-consuming work        
     if request.method == 'POST':
         sys.stdout.write('SUCCESS. 200 response will be sent')
+        sys.stdout.flush()
+        time.sleep(.2)  # or other time-consuming work        
         data = request.json
         sys.stdout.write(str(data))
+        sys.stdout.flush()
+        time.sleep(.2)  # or other time-consuming work        
         return request.json, 200
     else:
         abort(400)
